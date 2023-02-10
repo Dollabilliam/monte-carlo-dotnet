7e85b9db5e1fbb6976fdaaf44402970a583821f4
William Chelsvig <chels029@umn.edu>
2023-02-10 23:47:50 +0100

    add action block for processing scenarios

--- src/MonteCarloSimulator/ScenarioProcessor/ScenarioProcessor.cs
+++ src/MonteCarloSimulator/ScenarioProcessor/ScenarioProcessor.cs
@@ -1,3 +1,5 @@
+using System.Threading.Tasks.Dataflow;
+using MathNet.Numerics.Distributions;
 using MonteCarloSimulator.Queues.Messages;
 
 namespace MonteCarloSimulator.ScenarioProcessor;
@@ -16,5 +18,26 @@ public class ScenarioProcessor : IScenarioProcessor
 
     public async Task ProcessScenarios(QueueMessage message, CancellationToken cancellationToken)
     {
+        logger.LogInformation("Beginning processing scenarios for asset {AssetSimId}", message.SimulationObject.SimulationId);
+
+        var actions = new ActionBlock<SimulationObject>(
+            _ => ProcessSimulation(message.SimulationObject),
+            new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = 10,
+                CancellationToken = cancellationToken
+            });
+
+        for (var scenario = 0; scenario < message.SimulationObject.Scenarios; scenario++)
+        {
+            actions.Post(message.SimulationObject);
+        }
+        actions.Complete();
+        await actions.Completion;
+    }
+
+    private void ProcessSimulation(SimulationObject simulationObject)
+    {
+
     }
 }
